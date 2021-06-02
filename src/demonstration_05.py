d9a73e03f2bd208a01901b39a04d8f3d111af83f
bradley-krigbaum <bradley-krigbaum@lambdastudents.com>
2021-06-02 13:34:40 -0700

    demo 5 completed

--- src/demonstration_05.py
+++ src/demonstration_05.py
@@ -10,6 +10,26 @@ Examples:
 - sort_by_length(["may", "april", "september", "august"]) ➞ ["may", "april", "august", "september"]
 - sort_by_length([]) ➞ []
 """
-def sort_by_length(lst):
-    # Your code here
+def quick_sort_by_length(lst):
+    prepList = []
+    
+    for anyString in lst:
+        prepList.append( anyString )
+    return sorted(prepList)
 
+
+def sort_by_length( lst ):
+    newLst = lst
+    
+    for i in range( 1, len(newLst) ):
+        indexPlaceholder = newLst[i]
+        nextValue = i - 1
+        
+        while nextValue >= 0 and len(indexPlaceholder) < len(newLst[nextValue]):
+            newLst[nextValue + 1] = newLst[nextValue]
+            nextValue -= 1    
+        newLst[nextValue + 1] = indexPlaceholder
+    return newLst
+        
+        
+        
